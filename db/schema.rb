2a1e6711f755da55bfc06e245a4faaf2d340ca10
Yasuhiro Usutani <y_usutani@mac.com>
2020-04-26 09:31:35 +0900

    Tag validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_002820) do
+ActiveRecord::Schema.define(version: 2020_04_26_003028) do
 
   create_table "landmarks", force: :cascade do |t|
     t.string "name", null: false
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2020_04_26_002820) do
     t.string "answer3"
     t.integer "correct"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
