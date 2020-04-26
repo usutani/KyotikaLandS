26b55c75d6e0297fbeaba9e0872183d10d4d028f
Yasuhiro Usutani <y_usutani@mac.com>
2020-04-26 09:30:16 +0900

    Landmark Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+landmark = Landmark.create!(
+  name: 'JR京都駅',
+  hiragana: 'じぇーあーるきょうとえき',
+  latitude: 34.9855,
+  longitude: 135.758,
+  url: 'https://ja.wikipedia.org/wiki/京都駅',
+  question: '日本一長いホームとして知られるJR京都駅のホームの長さは？',
+  answer1: '558m',
+  answer2: '672m',
+  answer3: '773m',
+  correct: 1,
+  author: '臼谷泰弘'
+)
